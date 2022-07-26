9a29b99a43ab320a84e41d65b6c1e5682a546c78
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-07-26 07:47:33 +0200

    almost done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
 // Iteration 1: Names and Input
 //
+const hacker1 = "Albert";
+const hacker2 = "Mike";
+
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
+
+const longestName = (name1, name2) => {
+
+    let largest = [];
+    if (name1.lenght < name2.lenght) {
+        largest.push(name2);
+    } else {
+        largest.push(name1);
+    }
+    return `the largest name is ${largest}`;
+}
+
+console.log(longestName(hacker1, hacker2));
+
+
+const capitalNames = (name1, name2) => {
+    let capitals = name1.toUpperCase().split("").join(" ");
+    let capitals2 = name2.toUpperCase().split("").join(" ");
+    return `the capital name is ${capitals} & ${capitals2}`
+}
+
+console.log(capitalNames(hacker1, hacker2));
 // Iteration 2: Conditionals
 
 
